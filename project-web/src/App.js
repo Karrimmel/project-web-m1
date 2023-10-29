9289991327a6696a29a85f40caa640ab09b839e4
Dbataillard <damiebtd@gmail.com>
2023-10-29 16:34:18 +0100

    Books

--- project-web/src/App.js
+++ project-web/src/App.js
@@ -1,16 +1,17 @@
 import Books from './Compenants/Books';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {createBrowserRouter,RouterProvider } from 'react-router-dom';
 import './App.css';
+
+const route = createBrowserRouter([
+  {
+      path: "/",
+      element: <Books />,
+  }
+])
 
 export default function App() {
   return (
-    <div className="App">
-    <Router>
-        <Route exact path={"/"}>
-          <Books/>
-        </Route>
-    </Router>
-    </div>
+    <RouterProvider router={route} />
   );
 }
 
