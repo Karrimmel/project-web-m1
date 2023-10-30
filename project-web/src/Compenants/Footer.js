bdfde578fccda56d99726b16de6b18226ac71815
Karrimmel <karimelmestari@gmail.com>
2023-10-30 23:06:22 +0100

    Commente et link to

--- project-web/src/Compenants/Footer.js
+++ project-web/src/Compenants/Footer.js
@@ -1,40 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './css/Footer.css';
 
+/**
+ * Footer component that displays the footer section of the website.
+ * It includes links to various pages and a subscription input for newsletters.
+ */
 export default function Footer() {
   return (
     <div>
         <footer>
             <div className="content">
+                {/* Top section of the footer with logo and company name */}
                 <div className="top">
                     <div className="logo-details">
+                        {/* Logo image */}
                         <img src="/Logo_Solution_Factory_2.png" alt="La l'e-brairie" width="100" height="100" />
+                        {/* Company name */}
                         <div className="logo_name">La L'e-brairie</div>
                     </div>
                 </div>
 
+                {/* Link boxes for different categories */}
                 <div className="link-boxes">
+                    {/* Company information links */}
                     <ul className="box">
                         <li className="link_name">Company</li>
-                        <li><a href="/index-connected">Home</a></li>
-                        <li><a href="/">Contact us</a></li>
-                        <li><a href="/">About us</a></li>
-                        <li><a href="/">Get started</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Contact us</Link></li>
+                        <li><Link to="/">About us</Link></li>
+                        <li><Link to="/">Get started</Link></li>
                     </ul>
+                    {/* Services offered links */}
                     <ul className="box">
                         <li className="link_name">Services</li>
-                        <li><a href="/">App design</a></li>
-                        <li><a href="/">Web design</a></li>
-                        <li><a href="/">Logo design</a></li>
-                        <li><a href="/">Banner design</a></li>
+                        <li><Link to="/">App design</Link></li>
+                        <li><Link to="/">Web design</Link></li>
+                        <li><Link to="/">Logo design</Link></li>
+                        <li><Link to="/">Banner design</Link></li>
                     </ul>
+                    {/* Account related links */}
                     <ul className="box">
                         <li className="link_name">Account</li>
-                        <li><a href="/">Profile</a></li>
-                        <li><a href="/">My account</a></li>
-                        <li><a href="/">Preferences</a></li>
-                        <li><a href="/">Purchase</a></li>
+                        <li><Link to="/">Profile</Link></li>
+                        <li><Link to="/">My account</Link></li>
+                        <li><Link to="/">Preferences</Link></li>
+                        <li><Link to="/">Purchase</Link></li>
                     </ul>
+                    {/* Subscription box for newsletters */}
                     <ul className="box input-box">
                         <li className="link_name">Subscribe</li>
                         <li><input type="text" placeholder="Enter your email" /></li>
@@ -42,12 +55,17 @@ export default function Footer() {
                     </ul>
                 </div>
             </div>
+            {/* Bottom section of the footer with copyright and policy links */}
             <div className="bottom-details">
                 <div className="bottom_text">
-                    <span className="copyright_text">Copyright © 2021 <a href="/">CodingLab.</a>All rights reserved</span>
+                    {/* Copyright text */}
+                    <span className="copyright_text">
+                        Copyright © 2021 <Link to="/">CodingLab.</Link>All rights reserved
+                    </span>
+                    {/* Links to privacy policy and terms & conditions */}
                     <span className="policy_terms">
-                        <a href="/">Privacy policy</a>
-                        <a href="/">Terms & condition</a>
+                        <Link to="/">Privacy policy</Link>
+                        <Link to="/">Terms & condition</Link>
                     </span>
                 </div>
             </div>
--- project-web/src/index.js
+++ project-web/src/index.js
@@ -1,18 +1,28 @@
+// Importing the necessary modules from React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Importing the main stylesheet for the application
 import './index.css';
+// Importing the main App component
 import App from './App';
+// Importing the reportWebVitals function for measuring app performance
 import reportWebVitals from './reportWebVitals';
 
+// Creating a root DOM node for the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Rendering the App component inside the root DOM node
+// React.StrictMode is a tool for highlighting potential problems in an application
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/*
+  If you want to start measuring performance in your app, you can pass a function
+  to log results (for example: reportWebVitals(console.log)).
+  Alternatively, you can send the results to an analytics endpoint.
+  Learn more about measuring performance: https://bit.ly/CRA-vitals
+*/
 reportWebVitals();
