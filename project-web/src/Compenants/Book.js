a9a860c787dc5eddfd5f290d6375b705bc9a7c3f
Dbataillard <omenibus@gmail.com>
2023-10-30 17:41:37 +0100

    Book.js Done

--- project-web/src/Compenants/Book.js
+++ project-web/src/Compenants/Book.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './css/Book.css';
 import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -6,14 +7,14 @@ import { useQuery, gql } from '@apollo/client';
 
 
 const GET_MANGA_DETAILS = gql`
-  query GetMangaDetails($id: Int) { # Assurez-vous que le type de l'ID correspond à celui attendu par l'API
+  query GetMangaDetails($id: Int) { 
     Media(id: $id, type: MANGA) {
       id
       title {
         english
         native
       }
-      description(asHtml: false) # Mettez asHtml à true si vous voulez la description en HTML
+      description(asHtml: false) 
       coverImage {
         extraLarge
       }
@@ -42,7 +43,7 @@ export default function Book() {
   return (
     <div>
         <Navbar />
-        <div class="container">
+        <div class="containerBook">
             <div class="Book">
                 <img src={manga.coverImage.extraLarge} alt={`Cover of ${manga.title.english}`}/>
             </div>
@@ -54,7 +55,7 @@ export default function Book() {
                             <p class="author"></p>
                         </div>
                         <div class="rating">
-                            <p class="note">Note : {manga.averageScore}</p>
+                            <p class="note">Note : {manga.averageScore}/100</p>
                         </div>
                     </div>
                 </div>
