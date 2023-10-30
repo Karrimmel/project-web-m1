abe0a7ad0f4692c91dc36ce8ad90d2b04122344d
Karrimmel <karimelmestari@gmail.com>
2023-10-30 22:57:35 +0100

    app commente

--- project-web/src/App.js
+++ project-web/src/App.js
@@ -1,38 +1,45 @@
+// Importing necessary components and functions from libraries and files
 import Books from './Compenants/Books';
-import {createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
-import client from './config/apolloClient';
-import './App.css';
+import client from './config/apolloClient'; // Apollo Client for GraphQL API interaction
+import './App.css'; // Stylesheet for the App component
 import Book from './Compenants/Book';
 import Ebooks from './Compenants/Ebooks';
 
-
-
+// Creating a router with predefined routes for the application
 const route = createBrowserRouter([
   {
-      path: "/",
-      element: <Books />,
+    // Route for the home page, which will render the Books component
+    path: "/",
+    element: <Books />,
   },
   {
-      path:"/book/:id",
-      element: <Book />,
+    // Route for individual book details, which will render the Book component
+    // The ":id" is a URL parameter that will be used to fetch and display a specific book
+    path: "/book/:id",
+    element: <Book />,
   },
   {
-     path:"/e_book",
-     element: <Ebooks />,
+    // Route for the ebooks page, which will render the Ebooks component
+    path: "/e_book",
+    element: <Ebooks />,
   },
   {
-      path:"/e_book/genre/:genrename",
-      element:<Ebooks />,
+    // Route for filtering ebooks by genre, which will also render the Ebooks component
+    // The ":genrename" is a URL parameter that will be used to filter ebooks by a specific genre
+    path: "/e_book/genre/:genrename",
+    element: <Ebooks />,
   }
-])
+]);
 
+// The main App component that wraps the entire application
 export default function App() {
   return (
+    // ApolloProvider makes the Apollo Client available to all child components in the application
     <ApolloProvider client={client}>
+      {/* RouterProvider makes the routing configuration available to the application */}
       <RouterProvider router={route} />
     </ApolloProvider>
   );
 }
-
-
