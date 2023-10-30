8f67698fcdfd227524b1f8bd78de5f751c8aa4e1
Dbataillard <rom.dubois94@gmail.com>
2023-10-30 18:48:39 +0100

    Print the ebooks

--- project-web/src/Compenants/Ebooks.js
+++ project-web/src/Compenants/Ebooks.js
@@ -10,18 +10,36 @@ const GET_GENRES = gql`
   }
 `;
 
-export default function Ebooks() {
-  const { loading, error, data } = useQuery(GET_GENRES);
+const GET_MANGAS = gql`
+  query GetMangas {
+    Page(page: 1, perPage: 21) {
+      media(type: MANGA) {
+        id
+        title {
+          userPreferred
+        }
+        coverImage {
+          large
+        }
+      }
+    }
+  }
+`;
 
-  if (loading) {
+export default function Ebooks() {
+  const { loading: loadingGenres, error: errorGenres, data: dataGenres } = useQuery(GET_GENRES);
+  const { loading: loadingMangas, error: errorMangas, data: dataMangas } = useQuery(GET_MANGAS);
+
+  if (loadingGenres || loadingMangas) {
     return (
       <div className="spinner-container">
         <div className="spinner"></div>
         <p className="loading-message">Chargement des données...</p>
       </div>
-    );
-  }
-  if (error) return <p>Error: {error.message}</p>;
+  );
+}
+
+  if (errorGenres || errorMangas) return <p>Error: {(errorGenres || errorMangas).message}</p>;
 
   return (
     <div>
@@ -32,7 +50,7 @@ export default function Ebooks() {
                         <h3>Genres</h3>
                         <div className="list">
                             <li>Tous</li>
-                            {data.GenreCollection.map((genre, index) => (
+                            {dataGenres.GenreCollection.map((genre, index) => (
                               <div className="list" key={index}>
                                 <li>{genre}</li>
                               </div>
@@ -41,6 +59,11 @@ export default function Ebooks() {
                     </ul>
                 </div>
                 <div className="eBooks">
+                    {dataMangas.Page.media.map((manga) => (
+                        <div className="Book" key={manga.id}>                         
+                                <img src={manga.coverImage.large} alt={manga.title.userPreferred} />
+                        </div>
+                    ))}
                 </div>
         </div>
         <Footer />
