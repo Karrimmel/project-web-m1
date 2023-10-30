606a0cdb982b229b94f024eace66ec907171600b
Dbataillard <karimelmestari@gmail.com>
2023-10-30 22:34:13 +0100

    Ebook commente

--- project-web/src/Compenants/Ebooks.js
+++ project-web/src/Compenants/Ebooks.js
@@ -1,39 +1,18 @@
-import React, { useState } from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import './css/Ebooks.css';
-
-const GET_GENRES = gql`
-  query GetGenres {
-    GenreCollection
-  }
-`;
-
-const GET_MANGAS_BY_GENRE = gql`
-  query GetMangasByGenre($genre: String) {
-    Page(page: 1, perPage: 21) {
-      media(type: MANGA, genre: $genre) {
-        id
-        title {
-          userPreferred
-        }
-        coverImage {
-          large
-        }
-      }
-    }
-  }
-`;
-
+/**
+ * Ebooks component displays a list of manga genres and manga covers.
+ * It allows users to filter mangas by genre.
+ */
 export default function Ebooks() {
-  const [selectedGenre, setSelectedGenre] = useState('ALL'); // Utilisez 'ALL' pour indiquer tous les genres
+  // State to keep track of the selected genre for filtering
+  const [selectedGenre, setSelectedGenre] = useState('ALL');
+
+  // GraphQL queries to fetch genres and mangas by genre
   const { loading: loadingGenres, error: errorGenres, data: dataGenres } = useQuery(GET_GENRES);
   const { loading: loadingMangas, error: errorMangas, data: dataMangas } = useQuery(GET_MANGAS_BY_GENRE, {
     variables: { genre: selectedGenre === 'ALL' ? null : selectedGenre },
   });
 
+  // Display a loading spinner and message while data is being fetched
   if (loadingGenres || loadingMangas) {
     return (
       <div className="spinner-container">
@@ -43,12 +22,18 @@ export default function Ebooks() {
     );
   }
 
+  // Display an error message if there is an issue fetching data
   if (errorGenres || errorMangas) return <p>Error: {(errorGenres || errorMangas).message}</p>;
 
+  /**
+   * Handles clicks on genre filters.
+   * @param {string} genre - The genre to filter by.
+   */
   const handleGenreClick = (genre) => {
     setSelectedGenre(genre);
   };
 
+  // Filter out any unwanted genres such as 'Hentai' and 'Ecchi'
   const filteredGenres = dataGenres ? dataGenres.GenreCollection.filter(
     (genre) => genre !== 'Hentai' && genre !== 'Ecchi'
   ) : [];
@@ -61,7 +46,9 @@ export default function Ebooks() {
                 <ul className="genre">
                     <h3>Genres</h3>
                     <div className="list">
+                        {/* Clicking 'Tous' resets the genre filter */}
                         <li><Link to="/e_book" onClick={() => handleGenreClick('ALL')}>Tous</Link></li>
+                        {/* Map through the filtered genres to display genre links */}
                         {filteredGenres.map((genre, index) => (
                           <div className="list" key={index}>
                             <li>
@@ -75,8 +62,11 @@ export default function Ebooks() {
                 </ul>
             </div>
             <div className="eBooks">
+              {/* Display a loading message for mangas if they are being fetched */}
               {loadingMangas && <p>Loading mangas...</p>}
+              {/* Display an error message if there is an issue fetching mangas */}
               {errorMangas && <p>Error loading mangas: {errorMangas.message}</p>}
+              {/* Map through the fetched mangas and display their covers */}
               {dataMangas && dataMangas.Page.media.map((manga) => (
                   <div className="Book" key={manga.id}>
                       <Link to={`/book/${manga.id}`}>
